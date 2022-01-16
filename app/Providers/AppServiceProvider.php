c69931fc8ee456b321f587d7186adfb881a2159a
tanvir-sadi <tanvir.sadi10@northsouth.edu>
2022-01-16 18:09:07 +0600

    Default String Length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+        Schema::defaultStringLength(191);
     }
 }
